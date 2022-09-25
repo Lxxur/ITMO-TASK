f3b2a095d1adfcaf04033f6b132573dbcf652b6d
lui <luidza@inbox.ru>
2022-09-25 22:44:01 +0600

    add files

---
+++ main.py
@@ -0,0 +1,46 @@
+#TASK2
+# number = [10,20,30,40,50]
+# print(number)
+# number.reverse()
+# print(number)
+# n=int(input())
+
+# TASK3
+# n = int(input("введите число - "))
+# fact = 1
+# for i in range (1, n + 1):
+#     fact = fact * i
+# print("фактр", n, "=", fact)
+
+# TASK4
+# n = int(input("введите число -"))
+# sum=0
+# for n in range(1,n+1):
+#     sum=sum+n
+# print("sum",sum)
+
+# a= 5
+# for i in range(0, a + 1):
+#     for j in range(a- i, 0, -1):
+#         print(j, end=' ')
+#     print('')
+
+# n = int(input("введите число -"))
+# for i in range(n+1):
+#     for j in range(i):
+#         print(i, end=' ')
+#     print('')
+# n=int(input())
+# sum=0
+# for i in range(1,n+1):
+#     sum=sum+n
+#     n=n-1
+# print(sum)
+
+# a=int(input())
+# b=int(input())
+# get_discount=lambda a,b: a-(a*b/100)
+# print(get_discount(a,b))
+
+is_tpiplet=lambda a,b,c: a*a+b*b==c*c
+    if
